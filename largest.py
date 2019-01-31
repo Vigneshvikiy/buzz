22653679af65b609cc0ce92f2788e97e596366ed
Vigneshvikiy <46769037+Vigneshvikiy@users.noreply.github.com>
2019-01-31 19:13:24 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,9 @@
+a=int(input("enter first number:\n"))
+b=int(input("enter second number:\n"))
+c=int(input("enter third number:\n"))
+if(a>b)and(a>c):
+	            print("a is largest")
+if(b>a)and(b>c):
+	            print("b is largest")
+else:
+	 print("c is largest")
